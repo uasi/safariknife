325b4c744d706e316e2c5731b09a49f3c9c259e4
Tomoki Aonuma <uasi@uasi.jp>
2025-08-20 20:03:04 +0900

    chore: set target platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "safariknife",
+    platforms: [
+        .macOS(.v13)
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1")
     ],
